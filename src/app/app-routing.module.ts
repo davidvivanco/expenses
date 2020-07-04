a3182863e14f3bf4b67deed1634a4a45ccb543e8
davidvivanco <vivancomiquis@gmail.com>
2020-07-04 17:05:26 +0200

    welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('../pages/welcome/welcome.module').then(m => m.WelcomePageModule)
+  },
+  {
+    path: 'welcome',
+    loadChildren: () => import('../pages/welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('../pages/tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
